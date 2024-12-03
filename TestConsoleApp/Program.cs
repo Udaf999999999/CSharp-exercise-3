0b6897859292be718028966f6af8a9459b373d7c
Udaf999999999 <udaf999999999@yandex.ru>
2024-12-03 07:21:19 +0700

    Add type checking

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -8,10 +8,22 @@
 
             Console.WriteLine(MyName);
 
-            Console.WriteLine("Привет, мир\n");
-            Console.WriteLine("\tМне 27 лет");
-            Console.WriteLine("\tMy name is Jane");
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("\t Привет Мир");
+            Console.WriteLine("\t Мне 27 лет");
+            Console.WriteLine("\t My name is \n {0}", MyName);
+            Console.WriteLine("\u0040");
+            Console.WriteLine("\x23");
+
+            string name = "Jane";
+            ushort age = 27;
+            bool havePet = true;
+            float footSize = 78.5F;
+
+            Console.WriteLine("My name is " + name);
+            Console.WriteLine("My age is " + age);
+            Console.WriteLine("Do you have a pet? " + havePet);
+            Console.WriteLine("My foot size is " + footSize);
+
         }
     }
 }
